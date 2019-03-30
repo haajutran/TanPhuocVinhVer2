f194f7d2dedd1e28410384e73b5ac2dc47ac1415
Hau 50cm <haajutran@gmail.com>
2019-03-30 17:24:05 +0700

    Init

---
+++ TanPhuocVinh/TanPhuocVinh/ClientApp/src/components/Home.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Row, Col, Carousel, Card } from "antd";
+import i1 from "../assets/images/1.jpg";
+import i2 from "../assets/images/2.jpg";
+import i3 from "../assets/images/3.jpg";
+
+const { Meta } = Card;
+
+const Home = props => (
+  <div className="body">
+    <div className="carousel">
+      <h2>Tân Phước Vinh</h2>
+      <Carousel autoplay>
+        <div className="carousel-item">
+          <img src={i1} />
+        </div>
+        <div className="carousel-item">
+          <img src={i2} />
+        </div>
+        <div className="carousel-item">
+          <img src={i3} />
+        </div>
+      </Carousel>
+    </div>
+
+    <div className="functions">
+      <Row type="flex" justify="center">
+        <Col md={10} lg={7} xl={5}>
+          <Card
+            hoverable
+            style={{ width: 250 }}
+            cover={
+              <img
+                alt="example"
+                src="https://architecturebeast.com/wp-content/uploads/2014/08/Top_50_Modern_House_Designs_Ever_Built_featured_on_architecture_beast_26.jpg"
+              />
+            }
+          >
+            <Meta title="Thiết kế nhà" />
+          </Card>
+        </Col>
+        <Col md={10} lg={7} xl={5}>
+          <Card
+            hoverable
+            style={{ width: 250 }}
+            cover={
+              <img
+                alt="example"
+                src="https://s-ec.bstatic.com/images/hotel/max1024x768/681/68184730.jpg"
+              />
+            }
+          >
+            <Meta title="Thiết kế khách sạn" />
+          </Card>
+        </Col>
+        <Col md={10} lg={7} xl={5}>
+          <Card
+            hoverable
+            style={{ width: 250 }}
+            cover={
+              <img
+                alt="example"
+                src="https://t-ec.bstatic.com/images/hotel/max1024x768/889/88902727.jpg"
+              />
+            }
+          >
+            <Meta title="Thiết kế resort" />
+          </Card>
+        </Col>
+        <Col md={10} lg={7} xl={5}>
+          <Card
+            hoverable
+            style={{ width: 250 }}
+            cover={
+              <img
+                alt="example"
+                src="http://cdn-img-v2.webbnc.net/uploadv2/web/10/1093/product/2016/05/27/09/27/1464341530_phong-khach-noi-that-jhome-1.jpg"
+              />
+            }
+          >
+            <Meta title="Thiết kế nội thất" />
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  </div>
+);
+
+export default connect()(Home);
---
+++ TanPhuocVinh/TanPhuocVinh/ClientApp/src/components/Layout.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Layout, Menu } from "antd";
+const { Header, Content, Footer } = Layout;
+
+export default props => (
+  <div>
+    <Layout className="layout">
+      <Header className="top-navbar">
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["1"]}
+          style={{ lineHeight: "64px" }}
+        >
+          <Menu.Item key="1">Trang chủ</Menu.Item>
+          <Menu.Item key="2">Giới thiệu</Menu.Item>
+          <Menu.Item key="3">Tin tức</Menu.Item>
+        </Menu>
+      </Header>
+      <Content>{props.children}</Content>
+      <Footer style={{ textAlign: "center" }}>
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
+    </Layout>
+    ,
+  </div>
+);
